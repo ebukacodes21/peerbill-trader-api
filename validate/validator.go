88f06c774f1ad92fcb63bf7fb2ec06b03e44c5d0
ebukacodes21 <georgeokafo1@gmail.com>
2024-11-21 07:04:19 +0100

    added grpc params validation

--- validate/validator.go
+++ validate/validator.go
@@ -1,6 +1,14 @@
 package validate
 
-import "fmt"
+import (
+	"fmt"
+	"net/mail"
+	"regexp"
+)
+
+var (
+	isUsernameValid = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
+)
 
 func ValidateString(value string, min int, max int) error {
 	n := len(value)
@@ -12,15 +20,42 @@ func ValidateString(value string, min int, max int) error {
 }
 
 func ValidateFirstname(value string) error {
+	return ValidateString(value, 3, 100)
+}
+
+func ValidateLastname(value string) error {
+	return ValidateString(value, 3, 100)
+}
+
+func ValidateUsername(value string) error {
 	if err := ValidateString(value, 3, 100); err != nil {
 		return err
+	}
+	if !isUsernameValid(value) {
+		return fmt.Errorf("must contain only letters, digits or underscore")
 	}
 	return nil
 }
 
-func ValidateLastname(value string) error {
+func ValidatePassword(value string) error {
+	return ValidateString(value, 8, 100)
+}
+
+func ValidateEmail(value string) error {
 	if err := ValidateString(value, 3, 100); err != nil {
 		return err
 	}
+
+	if _, err := mail.ParseAddress(value); err != nil {
+		return fmt.Errorf("not a valid email address")
+	}
 	return nil
+}
+
+func ValidatePhone(value string) error {
+	return ValidateString(value, 11, 11)
+}
+
+func ValidateCountry(value string) error {
+	return ValidateString(value, 1, 30)
 }
